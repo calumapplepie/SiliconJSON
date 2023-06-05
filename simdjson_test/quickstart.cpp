60f9881313d022466b3c62c4aec0c44e98f543d0
Calum McConnell <mcconncm@lafayette.edu>
2023-06-05 14:47:53 -0400

    dump out raw tape for some analysis

--- simdjson_test/quickstart.cpp
+++ simdjson_test/quickstart.cpp
@@ -2,8 +2,7 @@
 #include "simdjson.h"
 using namespace simdjson;
 int main(void) {
-    ondemand::parser parser;
-    padded_string json = padded_string::load("twitter.json");
-    ondemand::document tweets = parser.iterate(json);
-    std::cout << uint64_t(tweets["search_metadata"]["count"]) << " results." << std::endl;
+    dom::parser parser;
+    dom::element json = parser.load("twitter.json");
+    json.dump_raw_tape(std::cout);
 }
