ab61b9bbbd5223cbfb45505ce05eafda0e7ade59
Calum McConnell <mcconncm@lafayette.edu>
2023-06-05 15:43:26 -0400

    use hex format

--- simdjson_test/quickstart.cpp
+++ simdjson_test/quickstart.cpp
@@ -2,16 +2,17 @@
 #include "simdjson.h"
 using namespace simdjson;
 
-#define TARGET_FILE_NAME twitter.json
+#define TARGET_FILE_NAME "twitter.json"
 
 void simdjson::dom::ScrewYouIWantTheTape(dom::element toStealFrom){
 	auto tape = toStealFrom.tape.doc;
 	
+	// code stolen almost verbatim from simdjson.h
 	uint32_t string_length;
   	size_t tape_idx = 0;
  	uint64_t tape_val = tape->tape[tape_idx];
  	uint8_t type = uint8_t(tape_val >> 56);
-	tape_idx++;
+	// tape_idx++;
   	size_t how_many = 0;
   	if (type == 'r') {
     		how_many = size_t(tape_val & internal::JSON_VALUE_MASK);
@@ -21,15 +22,16 @@ void simdjson::dom::ScrewYouIWantTheTape(dom::element toStealFrom){
 		std::abort();
 	}
 	
+	// now write the string
 	for (; tape_idx < how_many; tape_idx++) {
-		std::cout << tape->tape[tape_idx];
+		std::cout << std::hex << tape->tape[tape_idx];
 	}
 
 }
 
 int main(void) {
     dom::parser parser;
-    dom::element json = parser.load("TARGET_FILE_NAME");
+    dom::element json = parser.load(TARGET_FILE_NAME);
     dom::ScrewYouIWantTheTape(json);
     //json.dump_raw_tape(std::cout);
 }
