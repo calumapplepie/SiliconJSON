71dad2358c581ccd52b04a1c8f0034019a44958e
Calum McConnell <calumlikesapplepie@gmail.com>
2023-06-08 13:31:19 -0400

    Rough out dump for string tape from simdjson

--- simdjson_test/quickstart.cpp
+++ simdjson_test/quickstart.cpp
@@ -12,7 +12,9 @@ const std::string target_file_dir =  "../JsonTestFiles/";
 
 // Rest of the code should work without modifications
 
-void simdjson::dom::ScrewYouIWantTheTape(dom::element toStealFrom, std::ostream &toSendTo){
+void simdjson::dom::ScrewYouIWantTheTape(
+		const dom::element toStealFrom, std::ostream &structureTape, std::ostream &stringTape
+	){
 	// sneak in and steal the document
 	auto document = toStealFrom.tape.doc;
 	
@@ -35,12 +37,34 @@ void simdjson::dom::ScrewYouIWantTheTape(dom::element toStealFrom, std::ostream 
 	
 	// now write the tape out to a string
 	for (; tape_idx < how_many; tape_idx++) {
-		toSendTo << std::hex << document->tape[tape_idx];
+		const uint64_t curElement =  document->tape[tape_idx];
+		structureTape << std::hex << curElement;
 
 		#ifdef INCLUDE_LINE_BREAKS 
-			toSendTo <<std::endl;
-		#endif 
-		
+			structureTape <<std::endl;
+		#endif
+
+		// check if string element
+		const char elementType = curElement >> 56;
+		if (elementType == '"'){
+			// grab the payload
+			const uint64_t stringPos = curElement & 0x00FFFFFF;
+			// now we get to some stuff I took from dump_raw_json again
+			// but not quite, remember we still want hex
+			uint32_t string_length;
+			std::memcpy(&string_length, document->string_buf.get() + stringPos, sizeof(uint32_t));
+
+			// having done that nifty trick, we now go back to my code
+			// we need to include both the null ending byte and the size bytes.
+			// the only reason we don't just print out the whole tape... is because...
+			// wait they probably store an index to the last tape element somewhere...
+			for(uint32_t i = stringPos; i < stringPos + string_length + 5; i++){
+				stringTape << std::hex << document->string_buf[i];
+			}
+			
+			
+
+		}
 	}
 
 }
@@ -59,10 +83,16 @@ int main(void) {
 			continue;
 		}
     	dom::element json = parser.load(file.path().string());
-		auto hexPath = file.path();
-		hexPath.replace_extension(".hex");
-		std::ofstream hexOut{hexPath, std::ios_base::trunc} ;
-    	dom::ScrewYouIWantTheTape(json, hexOut);
+
+		auto hexPathStruct = file.path();
+		hexPathStruct.replace_extension("struct.hex");
+		auto hexPathString = file.path();
+		hexPathString.replace_extension("string.hex");
+
+		std::ofstream hexOutStruct{hexPathStruct, std::ios_base::trunc} ;
+		std::ofstream hexOutString{hexPathString, std::ios_base::trunc} ;
+
+    	dom::ScrewYouIWantTheTape(json, hexOutStruct, hexOutString);
 	}
 	
     //json.dump_raw_tape(std::cout);
