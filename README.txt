fd0b0e79c7388a0b5fd612971f3f300cc2de7738
Calum McConnell <calumlikesapplepie@gmail.com>
2023-06-08 14:57:35 -0400

    FSM is ready for the worst JSON documents

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 Hello and welcome to my 2023 summer reasearch project!
 Todo: spiel more
 Todo: document how to remake Vivado projects from committed git source
+Todo: empty strings (need tests too)
--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/ParserFSM.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/ParserFSM.sv
@@ -27,7 +27,18 @@ module ParserFSM(
     output writingString, writeStructure
     );
 
-    logic lookForValue;
+    typedef enum {
+        Root,        // the first, starting state: we just found the document root
+        FindKey,     // waiting for the first quote of a key
+        StartKey,    // Found the key's start
+        ReadKey,     // Read the key
+        FindValue,   // Find the value
+        StartString, // Found the first quote of a string
+        ReadString,  // Read the string
+        Error} state_t;
+    state_t curState;
+    state_t nextState;
+
     logic [8:0] bracketDepth;
     JsonCharType curCharType,
 
@@ -36,13 +47,42 @@ module ParserFSM(
     );
 
     always @(posedge clk ) begin
-        if(rst){
-            writingString = 1'b0;
-            writeStructure = 1'b0;
-            lookForValue = 1'b0;
-            curElementType = root;
-        }
-
+        if(rst) curState <= Root;
+        else curState <= nextState;
     end
+
+    // next state determiner
+    always_comb begin
+        unique case(state) 
+            Root        : nextState = FindKey;
+            FindKey     : nextState = (curChar == "\"") ? StartKey  : FindKey;
+            StartKey    : nextState = ReadKey; //NOTE: Breaks on empty key
+            ReadKey     : nextState = (curChar == "\"") ? FindValue : ReadKey; // todo: escaped quotes
+            FindValue   : nextState = (curChar == "\"") ? ReadString : FindValue; // todo: check for colon
+            StartString : nextState = ReadString; // NOTE: breaks on empty value
+            ReadString  : nextState = (curChar == "\"") ? FindKey: ReadString;
+        endcase
+        if(curChar == "}") nextState = Root;
+    end
+
+
+    // our outputs
+    always_comb begin
+        writingString  = 1'b0;
+        writeStructure = 1'b0;
+        curElementType = curCharType;
+        unique case(state)
+            Root : begin
+                curElementType = root;
+                writeStructure = 1'b1;
+            end
+            StartKey, StartString   : writeStructure = 1'b1;
+            ReadKey, ReadString     : writingString  = 1'b1;
+            default: 
+            // some states have no output besides default
+            ;
+        endcase
+    end
+
 
 endmodule
