bb9f0da178a2f4ea5162fe1d75e2ebb415619359
Calum McConnell <calumlikesapplepie@gmail.com>
2023-06-09 12:09:51 -0400

    get debugger working so i can see the memory dump
    i can now confirm SIMD strings on the tape are null-terminated

--- simdjson_test/quickstart.cpp
+++ simdjson_test/quickstart.cpp
@@ -11,71 +11,73 @@ const std::string target_file_dir =  "../JsonTestFiles/";
 #define INCLUDE_LINE_BREAKS 1
 
 // Rest of the code should work without modifications
+namespace simdjson {
+namespace dom {
+	void ScrewYouIWantTheTape(
+			const dom::element toStealFrom, std::ostream &structureTape, std::ostream &stringTape
+		){
+		// sneak in and steal the document
+		auto document = toStealFrom.tape.doc;
+		
+		// code stolen almost verbatim from simdjson.h
+		uint32_t string_length;
+		uint32_t string_buf_pos =0;
+		size_t tape_idx = 0;
+		uint64_t tape_val = document->tape[tape_idx];
+		uint8_t type = uint8_t(tape_val >> 56);
+		// tape_idx++;
 
-void simdjson::dom::ScrewYouIWantTheTape(
-		const dom::element toStealFrom, std::ostream &structureTape, std::ostream &stringTape
-	){
-	// sneak in and steal the document
-	auto document = toStealFrom.tape.doc;
-	
-	// code stolen almost verbatim from simdjson.h
-	uint32_t string_length;
-	uint32_t string_buf_pos =0;
-  	size_t tape_idx = 0;
- 	uint64_t tape_val = document->tape[tape_idx];
- 	uint8_t type = uint8_t(tape_val >> 56);
-	// tape_idx++;
+		// get length of array (yes we trust this data)
+		size_t how_many = 0;
+		if (type == 'r') {
+				how_many = size_t(tape_val & internal::JSON_VALUE_MASK);
+		}
+		else{
+			std::cout << "wtf you had one job! gimme a valid json document";
+			std::abort();
+		}
+		
+		// now write the tape out to a string
+		for (; tape_idx < how_many; tape_idx++) {
+			const uint64_t curElement =  document->tape[tape_idx];
+			structureTape << std::hex << curElement;
 
-	// get length of array (yes we trust this data)
-  	size_t how_many = 0;
-  	if (type == 'r') {
-    		how_many = size_t(tape_val & internal::JSON_VALUE_MASK);
-  	}
-	else{
-		std::cout << "wtf you had one job! gimme a valid json document";
-		std::abort();
-	}
-	
-	// now write the tape out to a string
-	for (; tape_idx < how_many; tape_idx++) {
-		const uint64_t curElement =  document->tape[tape_idx];
-		structureTape << std::hex << curElement;
-
-		#ifdef INCLUDE_LINE_BREAKS 
-			structureTape <<std::endl;
-		#endif
-
-		// check if string element
-		const char elementType = uint8_t(curElement >> 56);
-		if (elementType == '"'){
-			// grab the payload
-			const uint64_t stringPos = curElement & 0x00FFFFFF;
-			// now we get to some stuff I took from dump_raw_json again
-			// but not quite, remember we still want hex
-			std::memcpy(&string_length, document->string_buf.get() + stringPos, sizeof(uint32_t));
-
-			// having done that nifty trick, we now go back to my code
-			// we need to include both the null ending byte and the size bytes.
-			// the only reason we don't just print out the whole tape... is because...
-			// as far as i can tell, they throw away the length of the buffer when
-			// they're done parsing.
-			if(string_buf_pos != stringPos){
-				std::cout << "ERROR! String buffer position not as expected" <<std::endl;
-			}
-
-			for(string_buf_pos = stringPos; string_buf_pos < stringPos + string_length + 5; string_buf_pos++){
-				// that little plus promotes it to a real number!
-				stringTape << std::hex << +(document->string_buf[string_buf_pos]);
-			}
-			
 			#ifdef INCLUDE_LINE_BREAKS 
-				stringTape <<std::endl;
+				structureTape <<std::endl;
 			#endif
 
-		}
-	}
+			// check if string element
+			const char elementType = uint8_t(curElement >> 56);
+			if (elementType == '"'){
+				// grab the payload
+				const uint64_t stringPos = curElement & 0x00FFFFFF;
+				// now we get to some stuff I took from dump_raw_json again
+				// but not quite, remember we still want hex
+				std::memcpy(&string_length, document->string_buf.get() + stringPos, sizeof(uint32_t));
 
-}
+				// having done that nifty trick, we now go back to my code
+				// we need to include both the null ending byte and the size bytes.
+				// the only reason we don't just print out the whole tape... is because...
+				// as far as i can tell, they throw away the length of the buffer when
+				// they're done parsing.
+				if(string_buf_pos != stringPos){
+					std::cout << "ERROR! String buffer position not as expected" <<std::endl;
+				}
+
+				for(string_buf_pos = stringPos; string_buf_pos < stringPos + string_length + 5; string_buf_pos++){
+					// that little plus promotes it to a real number!
+					stringTape << std::hex << +(document->string_buf[string_buf_pos]);
+				}
+				
+				#ifdef INCLUDE_LINE_BREAKS 
+					stringTape <<std::endl;
+				#endif
+
+			}
+		}
+
+	}
+}}
 
 int main(void) {
     dom::parser parser;
