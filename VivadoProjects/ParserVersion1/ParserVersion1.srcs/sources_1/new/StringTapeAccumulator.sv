76943385abe9a8a62204ac940fb31107f84105b1
Calum McConnell <mcconncm@lafayette.edu>
2023-06-15 19:28:07 -0400

    rework string tape to use block ram

--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/StringTapeAccumulator.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/StringTapeAccumulator.sv
@@ -25,45 +25,80 @@ module StringTapeAccumulator
     (
         input UTF8_Char nextStringByte,
         output TapeIndex startIndex,
-        output logic [7:0] tape [StringTapeLength],
         input clk, rst, enable
     );
-    typedef logic [31:0] StringLength;
-    StringLength strLen;
-    TapeIndex curIndex;
-    logic wasEnabled;
+typedef logic [31:0] StringLength;
+StringLength strLen;
+TapeIndex curIndex, addressA, addressB;
+logic [1:0] cyclesDisabled;
+UTF8_Char byteA, byteB;
 
-    always @(posedge clk ) begin
-        if(rst) begin
-            foreach(tape[i]) tape[i] = '0;
-            curIndex = '0;
-            wasEnabled = '0;
-            startIndex = '0;
-        end else if (enable) begin
-            if(! wasEnabled) begin
-                curIndex += 56'd4;;
-                wasEnabled = 1'b1;
-                strLen = 32'b0;
+
+
+TapeBlockRam #(.WORDSIZE(8),.NUMWORDS(StringTapeLength)) ram (
+    .clk(clk), .ena('1), .enb('1), 
+    .wea(enable || cyclesDisabled != 3), .web(!enable && cyclesDisabled != 3), // we gotta write 5 bytes after string ends
+    .addra(addressA), .addrb(addressB),
+    .dia(byteA), .dib(byteB)
+);
+
+always_comb begin
+    // setting these to X might make my circuit more efficent (if vivado is cool),
+    // and should show me any bugs nice and clearly
+    byteA = 'x;
+    byteB = 'x;
+    addressA = 'x;
+    addressB = 'x;
+    if(enable) begin
+        byteA = nextStringByte;
+        addressA = curIndex;
+    end else begin
+        case(cyclesDisabled)
+            0 : begin
+                addressA = curIndex;
+                byteA = '0;
+                addressB = startIndex;
+                byteB = {strLen[ 7: 0]}; end
+            1 : begin
+                addressA = startIndex + 1;
+                byteA = {strLen[15: 8]};
+                addressB = startIndex + 2;
+                byteB = {strLen[23:16]};
             end
-            strLen++;
-            tape[curIndex] = nextStringByte;
-            curIndex++;
-            
-        end else begin
-            if(wasEnabled) begin
-                // Each string starts with the length of the string, in a 32 byte big-endian integer
-                tape[startIndex  ] = {strLen[ 7: 0]};
-                tape[startIndex+1] = {strLen[15: 8]};
-                tape[startIndex+2] = {strLen[23:16]};
-                tape[startIndex+3] = {strLen[31:24]};
-                // add null byte
-                tape[curIndex] = 8'b0;
-                curIndex++;
-                startIndex = curIndex;
-                wasEnabled = 1'b0;
+            2 : begin
+                addressA = startIndex + 1;
+                byteA = {strLen[15: 8]};
+                addressB = startIndex + 3;
+                byteB =  {strLen[31:24]};
             end
-        end
+        endcase;
     end
+end
+
+always_ff @(posedge clk ) begin
+    if(rst) begin
+        //foreach(tape[i]) tape[i] = '0;
+        curIndex <= 4'd4;
+        cyclesDisabled <= '0;
+        startIndex <= '0;
+        strLen <= 0;
+    end else if (enable) begin
+        curIndex <= curIndex + 1;
+        strLen <= strLen +1;
+    end else begin
+        if(cyclesDisabled < 3)begin
+            cyclesDisabled <= cyclesDisabled +1;
+        end
+        if (cyclesDisabled == 1) begin
+            curIndex <= curIndex +1;
+        end else if (cyclesDisabled == 2) begin
+            startIndex <= curIndex;
+            strLen <=0;
+            curIndex <= curIndex + 4;
+        end
+        
+    end
+end
 
     
 
