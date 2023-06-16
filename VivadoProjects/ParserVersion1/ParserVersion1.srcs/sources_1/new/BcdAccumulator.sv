49f3868d68ba11d8fbbfd4fd5375247cf92654b9
Calum McConnell <mcconncm@lafayette.edu>
2023-06-16 17:49:42 -0400

    work on the number accumulator
    look ma I read IEEE 1800

--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/BcdAccumulator.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/BcdAccumulator.sv
@@ -20,7 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module BcdAccumulator(
-
+module BcdAccumulator import Bcd::*; (
+        input Core::UTF8_Char curChar, 
+        input logic[2:0] selectedArray,// one-hot encoding plz
+        input clk, rst, enb,
+        output logic[63:0] accumulatedBufferData [2:0]
     );
+    BcdDigit[20:0] buffers [2:0];
+    BcdDigit nextDigit;
+    assign nextDigit = CharToBcd(curChar);
+    
+    // fancy schmany instance array
+    ShiftRegister #(.WORDSIZE(4), .NUMWORDS(20))  bufferHandler[2:0](
+        .clk(clk), .rst(rst), .enb(selectedArray), .arrayOut(buffers), .nextIn(nextDigit)
+    );
+    
+    assign accumulatedBufferData[0] = BcdArrayToBinary(buffers[0]);
+    assign accumulatedBufferData[1] = BcdArrayToBinary(buffers[1]);
+    assign accumulatedBufferData[2] = BcdArrayToBinary(buffers[2]);
+    
+    
+    
 endmodule
