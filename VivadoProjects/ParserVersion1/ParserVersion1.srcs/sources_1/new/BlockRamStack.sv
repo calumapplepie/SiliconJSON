34c6b07edda32a081cf7e5ea36fe35646e86658c
Calum McConnell <mcconncm@lafayette.edu>
2023-06-20 14:03:02 -0400

    write the stack data structure

--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/BlockRamStack.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/BlockRamStack.sv
@@ -20,14 +20,32 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module BlockRamStack(
-
+// values picked to use a single RAMB18E1 ; ie, half of a block ram element
+// see UG473.
+module BlockRamStack #(WIDTH=18, DEPTH=512)(
+        input clk, rst, enb,
+        input pushEnable, popTrigger,
+        input [WIDTH-1:0] pushData,
+        output logic [WIDTH-1:0] popData,
+        output logic [9:0] curDepth
     );
-    // values picked to use a single RAMB18E1 ; ie, half of a block ram element
-    // see UG473.
+    
+    // note: we never write to the lowest index of the stack.  
+    logic[9:0] curTop = 0;
+    logic[9:0] nextTop;
+    assign nextTop = curTop + 1'd1;
+    assign curDepth = curTop;
+    
     // Sadly, the internal FIFO logic controlers do not allow for FILO logic.  I checked :(
-    TapeBlockRam ram #(.WORDSIZE(18), .NUMWORDS(512)) (
-        
+    TapeBlockRam #(.WORDSIZE(WIDTH), .NUMWORDS(DEPTH)) ram (
+        .clk, .rst, .enb('1), .ena('1), .wea(pushEnable), .web('0),
+        .dia(pushData), .dob(popData), .addra(nextTop), .addrB(curTop)
     );
+    
+    always_ff @(posedge clk) begin
+        if(rst)              curTop <= '0;
+        else if(pushEnable)  curTop <= nextTop;
+        else if(popTrigger)  curTop <= curTop -1;
+    end
     
 endmodule
