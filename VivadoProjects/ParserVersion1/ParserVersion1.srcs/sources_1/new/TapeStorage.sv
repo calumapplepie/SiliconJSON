2b37c7ea82dda9c83950019b8aa472781af05004
Calum McConnell <mcconncm@lafayette.edu>
2023-07-05 15:37:08 -0400

    Start new modules for factoring out

---
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/TapeStorage.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/05/2023 03:36:42 PM
+// Design Name: 
+// Module Name: TapeStorage
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TapeStorage(
+
+    );
+endmodule
