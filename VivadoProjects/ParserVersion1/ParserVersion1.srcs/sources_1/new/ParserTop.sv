272dab493146e7568868d0fe62f99f68a9621e7d
Calum (probably) <mcconncm@lafayette.edu>
2023-07-07 14:41:23 -0400

    Refactor out to a new top module

---
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/ParserTop.sv
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/07/2023 04:22:05 PM
+// Design Name: 
+// Module Name: toplevel
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ParserTop
+    import Core::UTF8_Char, Core::StructTapeLength, Core::StringTapeLength, Core::JsonTapeElement, Core::ElementType;
+    (
+        input UTF8_Char curChar,
+        input clk, rst, enable,
+        BlockRamConnection.user stringRam, structRam
+    );
+    ElementType curElementType;
+    JsonTapeElement numberSecondElement;
+    wire writeString, writeStructure, clk, characterEscaped;
+    wire [23:0] keyValuePairs;
+    
+    
+    // our modules!
+    
+    ParserFSM parser (
+        .curChar (curChar), .curElementType(curElementType), .keyValuePairsSoFar(keyValuePairs),
+        .writeString, .writeStructure, .numberSecondElement, .characterEscaped,
+        .clk(clk), .rst(rst), .enb(enable)
+    );
+    
+    // this needs to be a pipeline for proper functionality
+    // FSM's current state describes what should be done with previous character
+    UTF8_Char lastChar;
+    logic lastCharacterEscaped;
+    ElementType lastElementType;
+    JsonTapeElement lastSecondElement;
+    always_ff @(posedge clk ) begin
+        if(rst) begin
+            lastChar             <= "{";
+            lastCharacterEscaped <= '0;
+            lastElementType      <= Core::objOpen;
+            lastSecondElement    <= '0;
+        end
+        else if(enable) begin
+            lastChar             <= curChar;
+            lastCharacterEscaped <= characterEscaped;
+            lastElementType      <= curElementType;
+            lastSecondElement    <= numberSecondElement;
+        end 
+    end
+    
+    TapeWriter writer (
+        .curChar(lastChar), .curElementType(lastElementType),
+        .writeString(writeString), .writeStructure(writeStructure), 
+        .keyValuePairs(keyValuePairs), .numberSecondElement(lastSecondElement),
+        .clk(clk),.rst(rst), .hash(LD0), .characterEscaped(lastCharacterEscaped),
+        .stringRam, .structRam, .enable
+    );
+endmodule
--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/toplevel.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/toplevel.sv
@@ -1,79 +1,14 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/07/2023 04:22:05 PM
-// Design Name: 
-// Module Name: toplevel
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-(* keep_hierarchy = "yes" *) module TopLevel
-    import Core::UTF8_Char, Core::StructTapeLength, Core::StringTapeLength, Core::JsonTapeElement, Core::ElementType;
-    (
+module TopLevel import Core::UTF8_Char; (
     input UTF8_Char curChar,
-    input GCLK, rst, enable,
-    output logic LD0, LD1
-    );
-    ElementType curElementType;
-    JsonTapeElement numberSecondElement;
-    wire writeString, writeStructure, clk, characterEscaped;
-    wire [23:0] keyValuePairs;
-    
+    input GCLK, rst, enable
+);
     assign clk = GCLK;
     
-    // our modules!
-    
-    ParserFSM parser (
-        .curChar (curChar), .curElementType(curElementType), .keyValuePairsSoFar(keyValuePairs),
-        .writeString, .writeStructure, .numberSecondElement, .characterEscaped,
-        .clk(clk), .rst(rst), .enb(enable)
-    );
-    
-    // this needs to be a pipeline for proper functionality
-    // FSM's current state describes what should be done with previous character
-    UTF8_Char lastChar;
-    logic lastCharacterEscaped;
-    ElementType lastElementType;
-    JsonTapeElement lastSecondElement;
-    always_ff @(posedge clk ) begin
-        if(rst) begin
-            lastChar             <= "{";
-            lastCharacterEscaped <= '0;
-            lastElementType      <= Core::objOpen;
-            lastSecondElement    <= '0;
-        end
-        else if(enable) begin
-            lastChar             <= curChar;
-            lastCharacterEscaped <= characterEscaped;
-            lastElementType      <= curElementType;
-            lastSecondElement    <= numberSecondElement;
-        end 
-    end
+    ParserTop parser (.*);
     
     BlockRamConnection #(.WORDSIZE(8), .ADDRWIDTH(14)) stringRam ();
     BlockRamConnection #(.WORDSIZE(64))                structRam     ();
-    
-    TapeWriter writer (
-        .curChar(lastChar), .curElementType(lastElementType),
-        .writeString(writeString), .writeStructure(writeStructure), 
-        .keyValuePairs(keyValuePairs), .numberSecondElement(lastSecondElement),
-        .clk(clk),.rst(rst), .hash(LD0), .characterEscaped(lastCharacterEscaped),
-        .stringRam, .structRam, .enable
-    );
-    
-    
-    TapeStorage storage ( .* );
+
+    TapeStorage storage ( .*);
+
 endmodule
