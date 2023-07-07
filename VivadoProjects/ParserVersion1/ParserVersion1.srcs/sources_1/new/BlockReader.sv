4609b01ff4e3eca8049325fdca4477dbc8f5537a
Calum (probably) <mcconncm@lafayette.edu>
2023-07-07 15:19:42 -0400

    write a wonderfully elaborate bram reader
    and then realize that XyLinx has an existing API for this

---
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/BlockReader.sv
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/07/2023 02:45:05 PM
+// Design Name: 
+// Module Name: BlockReader
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BlockReader #(WORDSIZE=8)(
+        BlockRamConnection.user ram,
+        input logic clk, enb, rst,
+        output logic [WORDSIZE-1:0] data 
+    );
+    logic [ram.ADDRWIDTH -1 :0] curAddr;
+    
+    
+    always_comb begin
+        ram.enb = '0;
+        ram.ena = enb;
+        ram.wea = '0; ram.web = '0;
+        
+        ram.addra = curAddr;
+        ram.doa = data;
+    end
+    
+    always_ff @(posedge clk) begin
+        if(rst)      curAddr <= '0;
+        else if(enb) curAddr <= curAddr + 1;
+    end
+    
+endmodule
--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/toplevel.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/toplevel.sv
@@ -1,14 +1,20 @@
 module TopLevel import Core::UTF8_Char; (
     input UTF8_Char curChar,
-    input GCLK, rst, enable
+    input GCLK, rst, enable, readSide
 );
     assign clk = GCLK;
     
-    ParserTop parser (.*);
+    ParserTop parser (
+        .stringRam(readSide ? stringRam[1] : stringRam[0]), 
+        .structRam(readSide ? stringRam[1] : stringRam[0]),
+        .*
+    );
     
-    BlockRamConnection #(.WORDSIZE(8), .ADDRWIDTH(14)) stringRam ();
-    BlockRamConnection #(.WORDSIZE(64))                structRam     ();
+    BlockRamConnection #(.WORDSIZE(8), .ADDRWIDTH(14)) stringRam[1:0] ();
+    BlockRamConnection #(.WORDSIZE(64))                structRam[1:0]     ();
 
-    TapeStorage storage ( .*);
+    TapeStorage storage[1:0] ( .*);
+    
+    
 
 endmodule
