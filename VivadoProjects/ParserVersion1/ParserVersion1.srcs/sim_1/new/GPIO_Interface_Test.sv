d5245abb545b571364fe8bed5344ab24d3c88e57
Calum (probably) <mcconncm@lafayette.edu>
2023-07-11 14:07:15 -0400

    add structure of test

--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sim_1/new/GPIO_Interface_Test.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sim_1/new/GPIO_Interface_Test.sv
@@ -21,6 +21,49 @@
 
 
 module GPIO_Interface_Test(
-
+        
     );
+    logic clk, rst, enable, readSide;
+    Core::UTF8_Char curChar;
+    
+    TopWrapper DUV (.GCLK(clk), .rst, .enable, .readSide, .curChar, .curStructBits, .curStringBits);
+    
+    string testDoc1 = "{\"author\":\"calum\"}";
+    string testDoc2 = "{\"The Answer\": 42}";
+
+    
+    task runTest();
+        readSide <= '0;
+    
+    endtask
+    
+    
+     initial begin
+        // cycles are 10 ns long: inputs change on multiples of 10, while positive edges are 5ns later
+        forever begin
+            clk <= 1'b0; #5;
+            clk <= 1'b1; #5;
+        end
+
+    end
+
+    initial begin
+        enable <= '0;
+        rst <= 1'b1;
+        #10;
+        rst <= 1'b0;
+        #20;
+        runTest();
+        enable <= '0;
+        #30 ;
+        rst <= 1'b1; 
+        #40;
+        rst <= 0;
+        #50;
+        // run test twice to ensure reset doesn't leave residue
+        runTest();
+        enable <= '0;
+        #60;
+    end
+    
 endmodule
