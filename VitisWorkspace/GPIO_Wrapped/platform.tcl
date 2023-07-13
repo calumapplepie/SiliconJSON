b2046ad5bfb5e404d16681f18d711cde96d87602
Calum (probably) <mcconncm@lafayette.edu>
2023-07-13 16:07:24 -0400

    init vitis projects

---
+++ VitisWorkspace/GPIO_Wrapped/platform.tcl
@@ -0,0 +1,18 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct /home/user/SiliconJSON/VitisWorkspace/GPIO_Wrapped/platform.tcl
+# 
+# OR launch xsct and run below command.
+# source /home/user/SiliconJSON/VitisWorkspace/GPIO_Wrapped/platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {GPIO_Wrapped}\
+-hw {/home/user/SiliconJSON/VivadoProjects/ParserVersion1/GPIO_Cpu_Interface_wrapper.xsa}\
+-proc {ps7_cortexa9_0} -os {standalone} -out {/home/user/SiliconJSON/VitisWorkspace}
+
+platform write
+platform generate -domains 
+platform active {GPIO_Wrapped}
+platform generate
