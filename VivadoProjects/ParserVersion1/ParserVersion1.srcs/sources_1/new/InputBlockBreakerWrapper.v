df722bdc0655884b158a1596ff4d40db0e317078
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-07-20 16:05:18 -0400

    some fixes and tweaks to block breaker

--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/InputBlockBreakerWrapper.v
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/InputBlockBreakerWrapper.v
@@ -1,6 +1,7 @@
-module InputBlockBreakerWrapper(input wire [31:0] curCharBlock , wire clk, rst, enableIn,
+module InputBlockBreakerWrapper(input wire [7:0] [0:3] curCharBlock , wire clk, rst, enableIn,
         output wire enableOut, wire[7:0] curChar);
         
-        InputBlockBreaker BREAKY (curCharBlock, clk, rst,enableIn, enableOut, curChar);
+        InputBlockBreaker BREAKY ({curCharBlock[0],curCharBlock[1], curCharBlock[2], curCharBlock[3]}, 
+                                        clk, rst,enableIn, enableOut, curChar);
         
 endmodule
