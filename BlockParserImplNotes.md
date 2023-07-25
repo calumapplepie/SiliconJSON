e3ed2df4d9e45739cd3302fc31b2e685bd8a51c2
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-07-25 14:50:36 -0400

    work further on porting over: spend time trying to avoid adder

---
+++ BlockParserImplNotes.md
@@ -0,0 +1,1 @@
+They do tomfoolery with nibbles and lookups; which i mean, sure, cool, whatever.  But on an FPGA, 10 comparasions and 10 ORs is cheap.
--- VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/EscapedCharFinder.sv
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/EscapedCharFinder.sv
@@ -1,5 +1,16 @@
-module EscapedCharFinder import Block::TextBlock, Block::BitBlock; (input BitBlock backslashes, output BitBlock escapedChars);
+module EscapedCharFinder import Block::*; (
+    input BitBlock backslashes, input logic prev_escaped,
+    output BitBlock escapedChars, output logic next_escaped);
 // based on find_escaped_branchless in json_string_scanner.h
+// TODO: see if there is a way to do this without an adder.  I want to say yes.
+always_comb begin
+    automatic BitBlock backslash, escaped_chars;
+    
+    // pretend that the first character isn't a backslash if it was escaped by previous block
+    backslash = backslashes;
+    backslash[0] = backslash[0] & !prev_escaped;
+    escaped_chars = {backslash[BlockSizeBits-2:0], prev_escaped}; // they call this follows_escape
+end
 
 
 endmodule
