eca13ab344ab0d8ffb1f088d78510a35e9ff7531
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-07-25 18:49:00 -0400

    brain dump

---
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/QuadReaderBlockRam.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/25/2023 06:27:16 PM
+// Design Name: 
+// Module Name: QuadReaderBlockRam
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module QuadReaderBlockRam(
+
+    );
+endmodule
