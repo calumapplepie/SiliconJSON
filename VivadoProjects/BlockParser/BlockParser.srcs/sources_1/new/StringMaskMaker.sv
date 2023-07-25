43d4b76e2a7e92f2814d323c05c83fa927a59c20
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-07-25 15:57:49 -0400

    Build out string stage 1 top level

--- VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/StringMaskMaker.sv
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/StringMaskMaker.sv
@@ -1,3 +1,29 @@
-module StringMaskMaker import Block::*; (input TextBlock charBlock, output ScannedCharBlock scannedChar);
+module StringMaskMaker import Block::*; (
+        input clk, rst, enb, 
+        input TextBlock chars, 
+        output ScannedCharBlock scannedChars);
+    BitBlock backslashes, quotes, escapedChars, stringChars;
+    
+    logic prev_in_string, next_in_string;
+    logic prev_escaped,   next_escaped;
+    
+    always_comb begin
+        scannedChars = new(backslashes, quotes, escapedChars, stringChars);
+    end
+    
+    always_ff@(posedge clk) begin
+        if(rst)begin
+            prev_in_string <= '0;
+            prev_escaped   <= '0;
+        end else if(enb) begin
+            prev_in_string <= next_in_string;
+            prev_escaped <= next_escaped;
+        end
+    end
+    
+    QuoteFinder classifer       (.chars, .quotes, .backslashes);
+    EscapedCharFinder escapee   (.backslashes, .prev_escaped, .escapedChars, .next_escaped);
+    
+    
     
 endmodule
