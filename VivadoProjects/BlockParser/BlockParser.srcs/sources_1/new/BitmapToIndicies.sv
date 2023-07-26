f77a51915cae1f45158cf3b8743f944241898469
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-07-26 16:05:57 -0400

    maybe get a working looping bitmap to index translator working?

--- VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/BitmapToIndicies.sv
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/BitmapToIndicies.sv
@@ -7,12 +7,34 @@ module BitmapToIndicies import Block::*; (
         
         BitBlockIndex [3:0] dexes;
         
-        BitmapIndexDecoder DECODEY (.in(bitmap), .dexes);
-        BitmapOneCounter   ONESEY  (.in(bitmap), .numOnes);
+        BitBlockIndex offset;
+        BitBlock bitmap_cur;
         
+        assign holdPipeline = numOnes > 4;
+        
+        always_ff @(posedge clk) begin // handle re-running in case of pipeline stall
+            if(rst) begin
+                offset <= '0;
+                bitmap_cur <= '0;            
+            end
+            else if (enb) begin
+                if(!holdPipeline) begin
+                    bitmap_cur <= bitmap;
+                    offset     <= '0;
+                end else begin
+                    bitmap_cur <= bitmap_cur >> dexes[3];
+                    offset <= dexes[3] + offset; 
+                end
+            end
+        end
+        
+        BitmapIndexDecoder DECODEY (.in(bitmap_cur), .dexes);
+        BitmapOneCounter   ONESEY  (.in(bitmap_cur), .numOnes);
+        
+        // transform bitmap indexes to block indexes
         always_comb begin
             for(int i = 0; i < 4; i++) begin
-                oneIndexes[i] = dexes[i] + startingIndex;
+                oneIndexes[i] = dexes[i] + startingIndex + offset;
             end
         end
         
