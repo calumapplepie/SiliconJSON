7d3f9a5c5378c0799c1a20ac4a4fecb792ede4f5
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-07-27 12:10:54 -0400

    add two new files for handling generic reading and writing of BRAMs

---
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/GenericBramRecorder.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/27/2023 11:57:24 AM
+// Design Name: 
+// Module Name: GenericBramRecorder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module GenericBramRecorder(
+
+    );
+endmodule
