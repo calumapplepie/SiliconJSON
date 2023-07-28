4ad18e0362b3c8cbe633e27a0051dd507f66b5eb
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-07-28 13:19:11 -0400

    changes to reflect my growth as a writer

--- VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/GenericBramReader.sv
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/GenericBramReader.sv
@@ -19,33 +19,36 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-// Yes, this module has a ton of parameters and only two ways we combine them all.
-// Since I am open sourcing this, I should maximize the reusable details for others.
-// Perhaps I could even build a separate Block Ram Library, because of the amount of tomfoolery
-// I do with them.
-// This would be easier if SystemVerilog and Vivado included just a few more features (eg, virtual classes
-
-module GenericBramReader import Ram::*;  #(WORDSIZE=8, JUMPSIZE=1, type WriteType, type ReadType) (
+// Reads a possibly-asymetric BRAM.  ReadType and WriteType should be structs of the form typical in RamPkg.sv , NUMWORDS is the number
+// of words that are read at once (if RAM is asymetric), and WORDSIZE is the bitwidth of each word
+// Verilog's weak typing system makes the fact that the 'data' output is an array not really matter
+module GenericBramReader import Ram::*;  #(WORDSIZE=8, NUMWORDS=1, type WriteType, type ReadType) (
         output WriteType ramWrite, 
         input ReadType ramRead,
         input logic clk, enable, rst,
-        output logic [WORDSIZE-1:0] data 
+        output logic [WORDSIZE-1:0] [NUMWORDS-1:0] data 
     );
-    // get an actual number for this later
-    (* mark_debug = "true" *) logic [16:0] curAddr;
+    
+    (* mark_debug = "true" *) logic [$bits(ramWrite.addra)-1:0] curAddr;
     
     always_comb begin
+        // Disable port b and all writes.  Tie enable to the input to avoid ram output updating to the next address until the next
+        // enable cycle (curAddr is IntendedOutAddr(t)+1 during cycle t regardless of if the enable was dropped to low during the cycle)  
         ramWrite.enb = '0;
         ramWrite.ena = enable;
         ramWrite.wea = '0; ramWrite.web = '0;
         
         ramWrite.addra = curAddr;
         data = ramRead.doa;
+        
+        // set others to 'x to make the compiler not complain about unassigned vars, and maybe do a bit of optimization
+        ramWrite.dia = 'x; ramWrite.dib = 'x;
+        ramWrite.addrb = 'x;
     end
     
     always_ff @(posedge clk) begin
         if(rst)      curAddr <= '0;
-        else if(enable) curAddr <= curAddr + JUMPSIZE;
+        else if(enable) curAddr <= curAddr + NUMWORDS;
     end
     
 endmodule
