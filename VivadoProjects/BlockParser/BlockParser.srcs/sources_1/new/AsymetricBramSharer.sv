ec5630c64f604550673cd51dbdba7a4e6c7cd78f
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-07-28 15:35:51 -0400

    modify BlockRamSharer to produce BramSharer

---
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/AsymetricBramSharer.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/28/2023 03:30:08 PM
+// Design Name: 
+// Module Name: AsymetricBramSharer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// note: infers data widths from passed args
+module AsymetricBramSharer import Ram::*; #(NUMWORDS=64, type ReadType, type WriteType)  (
+        input clk, enb, input WriteType ramW, output ReadType ramR
+    );
+      
+    AsymetricBlockRam #(.WORDSIZE_IN($bits(ramW.dia)), .WORDSIZE_OUT($bits(ramR.doa)), .NUMWORDS(NUMWORDS)) blockRam (
+       .clk(clk), .ena(ramW.ena && enb), .enb(ramW.enb && enb), 
+       .wea(ramW.wea), .web(ramW.web), .addra(ramW.addra), .addrb(ramW.addrb), 
+        .dib(ramW.dib), .dia(ramW.dia), .doa(ramR.doa), .dob(ramR.dob)  
+    );
+    
+endmodule
