2ec5ba2b020bc5ed8dd38390f2f48c0a569a1942
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-08-01 15:20:20 -0400

    write recorder modules that are totally not just inverted readers

--- VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/AxiStreamRecorder.sv
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/AxiStreamRecorder.sv
@@ -20,7 +20,36 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module AxiStreamRecorder(
-
+module AxiStreamRecorder import Ram::*;  #(WORDSIZE=8, NUMWORDS=1, type WriteType=StringBlockRamWrite) (
+        output WriteType ramWrite, 
+        input logic clk, enable, rst, 
+        output logic[$bits(ramWrite.addra)-1:0] transferLen,
+        // AXI signals
+        output logic TREADY, 
+        input logic [WORDSIZE-1:0] [NUMWORDS-1:0] TDATA,
+        input logic TLAST, TVALID, TRESET
     );
+    
+    
+    logic reset, writeData, wasLast;
+    
+    assign reset = rst && !TRESET; // can the controller cause a reset, independent of global reset? I dunno.  Better to be safe tho.
+    assign writeData = TVALID && TREADY;  
+    assign TREADY = enable && !reset && !wasLast; 
+            
+    GenericBramRecorder #(.WORDSIZE(WORDSIZE), .NUMWORDS(NUMWORDS), .WriteType(WriteType)) reader (
+        .clk, .enable(writeData), .rst(reset), .data(TDATA), .ramWrite
+    );  
+        
+    always_ff @(posedge clk) begin
+        if(reset) begin
+            transferLen <= '0;  
+        end
+        else if (enable) begin
+            if(TLAST && writeData) begin
+                wasLast <= '1;
+            end
+            if(writeData) transferLen <= transferLen +1;
+        end
+    end
 endmodule
--- VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/GenericBramRecorder.sv
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/GenericBramRecorder.sv
@@ -20,7 +20,41 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module GenericBramRecorder(
-
+module GenericBramRecorder import Ram::*;  #(WORDSIZE=8, NUMWORDS=1, USEPORTS=1, type WriteType) (
+        output WriteType ramWrite, 
+        input logic clk, enable, rst, 
+        input logic [NUMWORDS-1:0] [WORDSIZE-1:0] data 
     );
+    
+    (* mark_debug = "true" *) logic [$bits(ramWrite.addra)-1:0] curAddr;
+    
+    always_comb begin
+        // Disable port b and all writes.  Tie enable to the input to avoid ram output updating to the next address until the next
+        // enable cycle (curAddr is IntendedOutAddr(t)+1 during cycle t regardless of if the enable was dropped to low during the cycle)  
+        
+        ramWrite.ena = enable;
+        ramWrite.wea = '1; ramWrite.web = '1;
+        
+        ramWrite.addra = curAddr;
+        
+        
+        if(USEPORTS ==1) begin
+            ramWrite.dia = data;
+            ramWrite.addrb = 'x;
+            ramWrite.enb = '0;
+            ramWrite.dib = 'x;
+        end else begin
+            ramWrite.addrb = curAddr + NUMWORDS/2;
+            ramWrite.enb = enable;
+            ramWrite.dia = data[NUMWORDS/2-1:0];
+            ramWrite.dib = data[NUMWORDS-1:NUMWORDS/2];
+        end
+        
+    end
+    
+    always_ff @(posedge clk) begin
+        if(rst)         curAddr <= 0;
+        else if(enable) curAddr <= curAddr + NUMWORDS;
+    end
+    
 endmodule
