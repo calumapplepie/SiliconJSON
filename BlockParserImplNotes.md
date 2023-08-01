8266e80c631282a44b09455993ef0e9a37bd3aa7
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-08-01 16:30:25 -0400

    Make some modules, too tired to think properly

--- BlockParserImplNotes.md
+++ BlockParserImplNotes.md
@@ -1,2 +1,7 @@
 They do tomfoolery with nibbles and lookups as part of their layout tape stage 1 building
 which i mean, sure, cool, whatever.  But on an FPGA, 10 comparasions and 10 ORs is cheap.
+
+Handlers say whether or not they're done, and output a 'half-control' that controls.. half of a struct
+or string or something writer.
+
+more work needed ofc.
---
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/StageTwoParser.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/01/2023 04:23:45 PM
+// Design Name: 
+// Module Name: StageTwoParser
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module StageTwoParser(
+
+    );
+endmodule
