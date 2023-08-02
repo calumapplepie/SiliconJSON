d6b240bf526afe1c79abf7dbff6ca12a65ac4eb0
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-08-02 17:41:16 -0400

    we're gonna be doing some top-down designing

--- VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/BlockParserTop.sv
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/BlockParserTop.sv
@@ -1,7 +1,13 @@
 `timescale 1ns / 1ps
 
 module BlockParserTop(
-        
+        input  wire clk,
+        input  wire [63:0] inputStreamData, [3:0] inputStreamTdest,
+        input  wire inputStreamTvalid, inputStreamTlast, inputStreamReset,
+        output wire inputStreamReady,
+        input  wire outputStreamReset, outputStreamReady,
+        output wire [63:0] outputStreamData, [3:0] outputStreamTdest, 
+        output wire outputStreamValid, outputStreamLast
     );
     AxiStreamRecorder #(.NUMWORDS(8)) INPUT ();
     
--- VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/BlockParserWrapper.v
+++ VivadoProjects/BlockParser/BlockParser.srcs/sources_1/new/BlockParserWrapper.v
@@ -21,11 +21,23 @@
 
 
 module BlockParserWrapper(
-        input wire axis_aclk,
-        input wire [63:0] s_axis_tdata, wire s_axis_tvalid, s_axis_tlast, s_axis_resetn,
+        input  wire axis_aclk,
+        input  wire [63:0] s_axis_tdata, wire [3:0] s_axis_tdest,
+        input  wire s_axis_tvalid, s_axis_tlast, s_axis_resetn, 
         output wire s_axis_tready,
-        input wire m_axis_resetn, m_axis_tready,
-        output wire [63:0] m_axis_tdata, wire m_axis_tvalid, m_axis_tlast
+        input  wire m_axis_resetn, m_axis_tready,
+        output wire [63:0] m_axis_tdata, wire [3:0] m_axis_tdest,
+        input  wire m_axis_tvalid, m_axis_tlast
+    );
+    BlockParserTop TOP (
+        // this may be the ugliest thing ever but its what we've gotta do
+        .clk(axis_aclk),
+        .inputStreamData(s_axis_tdata), .inputStreamTdest(s_axis_tdest),
+        .inputStreamTvalid(s_axis_tvalid), .inputStreamTlast(s_axis_tlast), .inputStreamReset(s_axis_resetn),
+        .inputStreamReady(s_axis_tready),
+        .outputStreamReset(m_axis_resetn), .outputStreamReady(m_axis_tready),
+        .outputStreamData(m_axis_tdata), .outputStreamTdest(m_axis_tdest), 
+        .outputStreamValid(m_axis_tvalid), .outputStreamLast(m_axis_tlast)
     );
     
     
