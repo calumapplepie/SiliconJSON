40e01d057a0781a67a4d6c12a2e41c721cd9856f
Calum McConnell <calumlikesapplepie@gmail.com>
2023-08-07 16:29:54 -0400

    adjust orchestrator

--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/DMAOrchestrator.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/DMAOrchestrator.sv
@@ -10,7 +10,7 @@ module DMAOrchestrator( input logic clk, rst, enb,
     // NOTE: currently we keep it stupid simple, stupid
     // design v1: non-parallel FSM
     // will need to revise to make it drive select inputs and run stages in parallel
-    typedef enum {START, READ_INPUT, STAGE1, STAGE2, OUTPUT_STR, OUTPUT_LAY, ERROR} state_t;
+    typedef enum {START, READ_INPUT, PARSE, OUTPUT, ERROR} state_t;
     state_t curState, nextState;
     
     always_ff @(posedge clk)begin
@@ -21,11 +21,9 @@ module DMAOrchestrator( input logic clk, rst, enb,
     always_comb begin
         case(curState) 
             START:      nextState = READ_INPUT;
-            READ_INPUT: nextState = inputDone     ? STAGE1     : READ_INPUT;
-            STAGE1:     nextState = stage1Done    ? STAGE2     : STAGE1;
-            STAGE2:     nextState = stage2Done    ? OUTPUT_STR : STAGE2;
-            OUTPUT_STR: nextState = outputStrDone ? OUTPUT_LAY : OUTPUT_STR;
-            OUTPUT_LAY: nextState = outputLayDone ? START      : OUTPUT_LAY;
+            READ_INPUT: nextState = inputDone     ? PARSE   : READ_INPUT;
+            PARSE:      nextState = parserDone    ? OUTPUT  : PARSE;
+            OUTPUT:     nextState = outputDone    ? START   : OUTPUT;
             default:    nextState = ERROR;
         endcase
         
@@ -33,18 +31,16 @@ module DMAOrchestrator( input logic clk, rst, enb,
     
     always_comb begin
         if(curState == START) begin // just reset 'em all here
-            {inputRst,stage1Rst,stage2Rst,outputStrRst,outputLayRst} = '1; 
+            {inputRst,parserRst,outputRst} = '1; 
         end
-        else {inputRst,stage1Rst,stage2Rst,outputStrRst,outputLayRst} = '0;
+        else {inputRst,parserRst,outputRst} = '0;
         
         // set enables to 0, then raise them as desired
-         {inputEnable, stage1Enable, stage2Enable, outputStrEnable, outputLayEnable} ='0;
+         {inputEnable, parserEnable, outputEnable} ='0;
          case(curState) 
             READ_INPUT: inputEnable     = '1;
-            STAGE1:     stage1Enable    = '1;
-            STAGE2:     stage2Enable    = '1;
-            OUTPUT_STR: outputStrEnable = '1;
-            OUTPUT_LAY: outputLayEnable = '1;
+            PARSE:      parserEnable    = '1;
+            OUTPUT:     outputEnable    = '1;
             // no need for default, all outputs always driven
          endcase
     
