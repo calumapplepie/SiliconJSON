16f41755db374d003e8489cc9c52ee85e3d868da
Calum McConnell <calumlikesapplepie@gmail.com>
2023-08-07 15:27:26 -0400

    build up DMA orchestrator more

--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/DMAOrchestrator.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/DMAOrchestrator.sv
@@ -1,26 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/07/2023 01:03:21 PM
-// Design Name: 
-// Module Name: DMAOrchestrator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module DMAOrchestrator(
-
+module DMAOrchestrator( input logic clk, rst, enb,
+    output logic       inputEnable, parserEnable, outputEnable,
+    output logic       inputRst,    parserRst,    outputRst,
+    input  logic       inputDone,   parserDone,   outputDone,  
+    output logic [3:0] inputBlock,  parserBlock,  outputBlock
     );
 endmodule
--- VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/DMATop.sv
+++ VivadoProjects/ParserVersion1/ParserVersion1.srcs/sources_1/new/DMATop.sv
@@ -19,10 +19,36 @@ module DMATop import Ram::*, Core::*; (
     StructBlockRamWrite parserLayWrite, outputLayWrite;
     StructBlockRamRead  outputLayRead;
 
-    logic       inputEnable, parserEnable, outputStrEnable, outputLayEnable;
-    logic       inputRst,    parserRst,    outputStrRst,    outputLayRst;
-    logic       inputDone,   parserDone,   outputStrDone,   outputLayDone;    
+    logic       inputEnable, parserEnable, outputEnable;
+    logic       inputRst,    parserRst,    outputRst;
+    logic       inputDone,   parserDone,   outputDone;    
     logic [3:0] inputBlock,  parserBlock,  outputBlock;
+    
+    InputIndex inputInLen,    parserInLen;                       // lengths of input tapes
+    InputIndex parserStrLen,  outputStrLen;                                 // lengths of string tape
+    InputIndex parserLayLen,  outputLayLen;                                 // lengths of structure tapes
+
+    DMAOrchestrator dma_mgr  ( .clk, .enb, .rst,
+        .inputEnable, .parserEnable, .outputEnable,
+        .inputRst,    .parserRst,    .outputRst,
+        .inputDone,   .parserDone,   .outputDone,
+        .inputBlock,  .parserBlock,  .outputBlock
+    );
+
+    LengthOrchestrator inputLenMgr(.clk, .web(inputDone), 
+        .din(inputInLen),  .dout1(parserInLen), .dout2(),
+        .selW(inputBlock), .selR1(parserBlock), .selR2()
+    );
+    
+    LengthOrchestrator strLenMgr(.clk, .web(parserDone), 
+        .din(parserStrLen), .dout1(outputStrLen), .dout2(),
+        .selW(parserBlock), .selR1(outputBlock),  .selR2()
+    );
+    
+    LengthOrchestrator layLenMgr(.clk, .web(parserDone), 
+        .din(parserLayLen), .dout1(outputLayLen), .dout2(),
+        .selW(parserBlock), .selR1(outputBlock),  .selR2()
+    );
 
 
 
