cf4b308eb727704ccdc62399ebbf54ce61ea694f
Calum McConnell (lab PC) <mcconncm@lafayette.edu>
2023-08-17 15:59:37 -0400

    make progress

--- VitisWorkspace/DmaTest/src/DmaJsonDriver.h
+++ VitisWorkspace/DmaTest/src/DmaJsonDriver.h
@@ -1,8 +1,12 @@
 #ifndef DMAJSONDRIVER_H_
 #define DMAJSONDRIVER_H_
 
+// Set up the driver and the ip
 void init_driver();
-void queue_doc();
+
+// send a json document to be parsed
+void queue_doc(Json_Doc &doc);
+// TODO: prioritization for the MCDMA IP, will need to support
 
 
 #endif /* DMAJSONDRIVER_H_ */
--- VitisWorkspace/DmaTest/src/JsonDoc.h
+++ VitisWorkspace/DmaTest/src/JsonDoc.h
@@ -2,15 +2,31 @@
 #define JSONDOC_H_ 1
 
 #include <string>
+#include <cstdint>
 
 class JsonDoc {
 public:
+	JsonDoc(std::string in){unparsed = in;};
 	const int   getUnparsedLen(){return unparsed.length();};
 	const char* getUnparsedBuffer(){return unparsed.c_str();};
 
+	// todo: encapsulate these
+	void setCharBuffer(uint8_t* chars, uint16_t len){
+		str_buf = chars;
+		str_buf_len = len;
+	};
+	void setLayoutBuffer(uint64_t* lay, uint16_t len){
+		lay_buf = lay;
+		lay_buf_len = len;
+	};
+
 
 private:
-	std::string unparsed;
+	const std::string unparsed;
+	char* str_buf;
+	uint16_t str_buf_len;
+	uint64_t lay_buf;
+	uint16_t lay_buf_len;
 };
 #endif /* JSONDOC_H_ */
 
